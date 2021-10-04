9d187899375bc85b6e534b95974079aef5f8d2c4
Jon Carstens <jjcarstens@me.com>
2021-10-04 17:45:06 -0600

    support drawing text and canvas

--- lib/scroll_hat/display.ex
+++ lib/scroll_hat/display.ex
@@ -6,7 +6,7 @@ defmodule ScrollHat.Display do
 
   use GenServer
 
-  @type canvas :: [[non_neg_integer()], ...]
+  @type canvas :: [[non_neg_integer()], ...] | String.t()
 
   alias Circuits.I2C
   alias ScrollHat.IS31FL3731, as: Driver
@@ -38,24 +38,35 @@ defmodule ScrollHat.Display do
   def clear(), do: GenServer.call(__MODULE__, :clear)
 
   @doc """
-  Draw a canvas matrix on the display
+  Draw a canvas matrix or text on the display
 
-  The canvas is expected to be a 2D 7x17 matrix where each value is
+  The canvas matrix is expected to be a 2D 7x17 matrix where each value is
   an integer between 0-255. The value represents the brightness of the
   LED at that location and any value > 0 signifies that the LED is on.
+
+  If the canvas is a binary, it will be transposed to a matrix based
+  on the font set with `set_font/1`
   """
-  @spec draw(canvas()) :: IS31FL3731.result()
-  def draw(canvas), do: GenServer.call(__MODULE__, {:draw, canvas})
+  @spec draw(canvas()) :: IS31FL3731.result() | {:error, :invalid_canvas}
+  def draw(canvas) when is_list(canvas) or is_binary(canvas) do
+    GenServer.call(__MODULE__, {:draw, canvas})
+  end
+
+  def draw(_), do: {:error, :invalid_canvas}
 
   @doc """
-  Draw a canvas and then scroll it to the left
+  Draw a canvas or text and then scroll it to the left.
 
   `step_time` is the number of milliseconds to wait between each step
+
+  See `draw/1` for details about `canvas`.
   """
-  @spec marquee(canvas(), non_neg_integer()) :: IS31FL3731.result()
-  def marquee(canvas, step_time) do
+  @spec marquee(canvas(), non_neg_integer()) :: IS31FL3731.result() | {:error, :invalid_canvas}
+  def marquee(canvas, step_time) when is_list(canvas) or is_binary(canvas) do
     GenServer.call(__MODULE__, {:marquee, canvas, step_time})
   end
+
+  def marquee(_, _), do: {:error, :invalid_canvas}
 
   @doc """
   Set the brightness for all active LEDs for the current canvas
@@ -112,18 +123,24 @@ defmodule ScrollHat.Display do
     {:reply, Driver.set_frame(state.i2c, @default_frame, @all_off), state}
   end
 
-  def handle_call({:draw, canvas}, _from, state) do
-    if state.timer, do: Process.cancel_timer(state.timer)
-    {:reply, do_draw(state.i2c, canvas), %{state | canvas: canvas}}
+  def handle_call({:draw, canvas_or_text}, _from, state) do
+    state =
+      state
+      |> maybe_cancel_timer()
+      |> set_canvas(canvas_or_text)
+
+    {:reply, do_draw(state), state}
   end
 
-  def handle_call({:marquee, canvas, step_time}, _from, state) do
-    if state.timer, do: Process.cancel_timer(state.timer)
+  def handle_call({:marquee, canvas_or_text, step_time}, _from, state) do
+    state =
+      %{state | step_time: step_time}
+      |> maybe_cancel_timer()
+      |> set_canvas(canvas_or_text)
+
     t = Process.send_after(self(), :shift, step_time)
 
-    state = %{state | timer: t, canvas: canvas, step_time: step_time}
-
-    {:reply, do_draw(state.i2c, canvas), state}
+    {:reply, do_draw(state), %{state | timer: t}}
   end
 
   def handle_call({:set_brightness, val}, _from, state) do
@@ -132,29 +149,33 @@ defmodule ScrollHat.Display do
     # to all be the same. Maybe in the future we support some sorratio
     # of brightness control?
     new = for row <- state.canvas || [], do: Enum.map(row, &if(&1 > 0, do: val, else: 0))
-    {:reply, do_draw(state.i2c, new), %{state | canvas: new, brightness: val}}
+
+    state =
+      %{state | brightness: val}
+      |> set_canvas(new)
+
+    {:reply, do_draw(state), state}
   end
 
   def handle_call({:set_font, font}, _from, state) do
-    state = %{
-      state
-      | font: font,
-        canvas: ScrollHat.Font.text_to_canvas(state.text, state.font, state.brightness)
-    }
+    state =
+      %{state | font: font}
+      |> set_canvas(state.text)
 
-    {:reply, do_draw(state.i2c, state.canvas), state}
+    {:reply, do_draw(state), state}
   end
 
   @impl GenServer
   def handle_info(:shift, state) do
     shifted = for [f | row] <- state.canvas, do: List.insert_at(row, -1, f)
-    do_draw(state.i2c, shifted)
+    state = set_canvas(state, shifted)
+    _ = do_draw(state)
     t = Process.send_after(self(), :shift, state.step_time)
 
-    {:noreply, %{state | canvas: shifted, timer: t}}
+    {:noreply, %{state | timer: t}}
   end
 
-  defp do_draw(i2c, canvas) do
+  defp do_draw(%{i2c: i2c, canvas: canvas}) do
     {bright_buff, led_buff} = create_buffers(canvas)
 
     # Set brightness (PWM)
@@ -192,5 +213,24 @@ defmodule ScrollHat.Display do
 
   defp bit_index(x, y) when x > 8 do
     16 * x + (6 - y) - 135
+  end
+
+  defp maybe_cancel_timer(%{timer: nil} = s), do: s
+
+  defp maybe_cancel_timer(s) do
+    _ = Process.cancel_timer(s.timer)
+    %{s | timer: nil}
+  end
+
+  defp set_canvas(state, text) when is_binary(text) do
+    %{
+      state
+      | text: text,
+        canvas: ScrollHat.Font.text_to_canvas(text, state.font, state.brightness)
+    }
+  end
+
+  defp set_canvas(state, canvas) when is_list(canvas) do
+    %{state | canvas: canvas}
   end
 end
